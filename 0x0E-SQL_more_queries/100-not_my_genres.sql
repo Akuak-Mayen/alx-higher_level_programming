0ca73256695c4e223dfb61a1d3057b5c733e0887
Akuak-Mayen <mayenakuak84@gmail.com>
2023-11-15 06:23:45 -0800

    not my genres

--- 0x0E-SQL_more_queries/100-not_my_genres.sql
+++ 0x0E-SQL_more_queries/100-not_my_genres.sql
@@ -1,10 +1,18 @@
--- lists all shows contained in hbtn_0d_tvshows that have at least one genre linked
--- Each record should display: tv_shows.title - tv_show_genres.genre_id
--- Results must be sorted in ascending order by tv_shows.title and tv_show_genres.genre_id
--- You can use only one SELECT statement
+-- lists all genres not linked to the show dexter
+-- The tv_shows table contains only one record where title = Dexter (but the id can be different)
+-- Each record should display: tv_genres.name
+-- Results must be sorted in ascending order by the genre name
+-- You can use a maximum of two SELECT statement
 
-SELECT tv_shows.title, tv_show_genres.genre_id
-FROM tv_show_genres
-INNER JOIN tv_shows ON tv_shows.id = tv_show_genres.show_id
-ORDER BY tv_shows.title, tv_show_genres.genre_id ASC;
+SELECT name 
+FROM tv_genres
+WHERE name NOT IN (
+    SELECT name 
+    FROM tv_genres
+    LEFT JOIN tv_show_genres ON tv_genres.id = tv_show_genres.genre_id
+    LEFT JOIN tv_shows ON tv_show_genres.show_id = tv_shows.id
+    WHERE tv_shows.title = 'Dexter'
+  )
+GROUP BY name
+ORDER BY name ASC;
 
